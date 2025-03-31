c2914ba5b2e462dc5c8407a97db9020ec922faa8
solify020 <sim68corwinqmr@hotmail.com>
2025-03-31 10:38:17 +0900

    fix

--- src/components/PathwaysSVG.tsx
+++ src/components/PathwaysSVG.tsx
@@ -7,14 +7,29 @@ function PathwaysSVG() {
     useEffect(() => {
         const handleMouseEnter = (event) => {
             const id = event.target.id;
+            if (id.startsWith("rect-") || id.startsWith("elipse-")) {
+                document.getElementById("text-first-1")?.classList.remove("always-show");
+                document.getElementById("text-box-first-1")?.classList.remove("always-show");
+                document.getElementById("pointer-first-1")?.classList.remove("always-show");
+                document.getElementById("text-first-1")?.classList.add("hide-1");
+                document.getElementById("text-box-first-1")?.classList.add("hide-1");
+                document.getElementById("pointer-first-1")?.classList.add("hide-1");
+            }
+            
             if (id.startsWith("rect-")) {
                 const identifier = id.replace("rect-", "");
+                document.getElementById(`pointer-${identifier}`)?.classList.remove("hide");
+                document.getElementById(`text-box-${identifier}`)?.classList.remove("hide");
+                document.getElementById(`text-${identifier}`)?.classList.remove("hide");
                 document.getElementById(`pointer-${identifier}`)?.classList.add("show");
                 document.getElementById(`text-box-${identifier}`)?.classList.add("show");
                 document.getElementById(`text-${identifier}`)?.classList.add("show");
             }
             else if(id.startsWith("elipse-")) {
                 const identifier = id.replace("elipse-", "");
+                document.getElementById(`pointer-${identifier}`)?.classList.remove("hide");
+                document.getElementById(`text-box-${identifier}`)?.classList.remove("hide");
+                document.getElementById(`text-${identifier}`)?.classList.remove("hide"); 
                 document.getElementById(`pointer-${identifier}`)?.classList.add("show");
                 document.getElementById(`text-box-${identifier}`)?.classList.add("show");
                 document.getElementById(`text-${identifier}`)?.classList.add("show"); 
@@ -28,12 +43,18 @@ function PathwaysSVG() {
                 document.getElementById(`pointer-${identifier}`)?.classList.remove("show");
                 document.getElementById(`text-box-${identifier}`)?.classList.remove("show");
                 document.getElementById(`text-${identifier}`)?.classList.remove("show");
+                document.getElementById(`pointer-${identifier}`)?.classList.add("hide");
+                document.getElementById(`text-box-${identifier}`)?.classList.add("hide");
+                document.getElementById(`text-${identifier}`)?.classList.add("hide");
             }
             else if(id.startsWith("elipse-")) {
                 const identifier = id.replace("rect-", "");
                 document.getElementById(`pointer-${identifier}`)?.classList.remove("show");
                 document.getElementById(`text-box-${identifier}`)?.classList.remove("show");
                 document.getElementById(`text-${identifier}`)?.classList.remove("show");
+                document.getElementById(`pointer-${identifier}`)?.classList.add("hide");
+                document.getElementById(`text-box-${identifier}`)?.classList.add("hide");
+                document.getElementById(`text-${identifier}`)?.classList.add("hide");
             }
         };
 
